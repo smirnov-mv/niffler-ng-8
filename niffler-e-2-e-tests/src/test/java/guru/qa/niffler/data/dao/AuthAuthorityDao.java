6749aac3f0a4276d8c63a826ea01028495d2de93
dtuchs <dtuchs@gmail.com>
2025-04-23 15:51:08 +0500

    Homeworks for streams #5.1-#5.2 - Spring JDBC - row mappers, DAO

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
@@ -2,8 +2,11 @@ package guru.qa.niffler.data.dao;
 
 import guru.qa.niffler.data.entity.auth.AuthorityEntity;
 
+import java.util.List;
+
 public interface AuthAuthorityDao {
 
   void create(AuthorityEntity... authority);
 
+  List<AuthorityEntity> findAll();
 }
