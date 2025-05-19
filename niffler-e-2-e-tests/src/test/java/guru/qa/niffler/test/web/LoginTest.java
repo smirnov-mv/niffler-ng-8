84f058012718c7ce894fd746682f94667e1d41a4
saidman <saidman@yandex.ru>
2025-05-19 03:36:59 +0300

    hw 3.1

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -0,0 +1,29 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.junit5.TextReportExtension;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith({BrowserExtension.class, TextReportExtension.class})
+public class LoginTest {
+
+  @Test
+  void mainPageShouldBeDisplayedAfterSuccessLogin() {
+    Selenide.open(LoginPage.URL, LoginPage.class)
+        .doLogin("111", "111")
+        .shouldBeLoaded();
+  }
+
+  @Test
+  void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+    Selenide.open(LoginPage.URL, LoginPage.class)
+        .setUserName("111")
+        .setPassword("222")
+        .btnLogInClick()
+        .shouldBeVisibleLoginErrorContainer()
+        .shouldHaveErrorUrl();
+  }
+}
