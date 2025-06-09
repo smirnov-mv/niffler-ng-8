6ede24ab88b949d67b867257830667e2f60f1131
dtuchs <dtuchs@gmail.com>
2025-06-09 23:43:27 +0700

    stream #10.2 - Implementation for friendship creation, JUnit parallel stream

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/model/TestData.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/model/TestData.java
@@ -4,5 +4,24 @@ import java.util.List;
 
 public record TestData(String password,
                        List<CategoryJson> categories,
-                       List<SpendJson> spendings) {
+                       List<SpendJson> spendings,
+                       List<UserJson> friends,
+                       List<UserJson> outcomeInvitations,
+                       List<UserJson> incomeInvitations) {
+
+  public List<String> friendsUsernames() {
+    return extractUsernames(friends);
+  }
+
+  public List<String> outcomeInvitationsUsernames() {
+    return extractUsernames(outcomeInvitations);
+  }
+
+  public List<String> incomeInvitationsUsernames() {
+    return extractUsernames(incomeInvitations);
+  }
+
+  private List<String> extractUsernames(List<UserJson> users) {
+    return users.stream().map(UserJson::username).toList();
+  }
 }
