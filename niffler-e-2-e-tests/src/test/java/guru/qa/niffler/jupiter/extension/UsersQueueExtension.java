13869b6c0546d7280893d2f4fdc0b00910f88af2
saidman <saidman@yandex.ru>
2025-09-26 02:30:46 +0300

    Refactor UsersQueueExtension to simplify user retrieval logic

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
@@ -59,12 +59,7 @@ public class UsersQueueExtension implements
               Optional<StaticUser> user = Optional.empty();
               StopWatch sw = StopWatch.createStarted();
               while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
-                user = switch (ut.value()) {
-                  case EMPTY -> Optional.ofNullable(EMPTY_USERS.poll());
-                  case WITH_FRIEND -> Optional.ofNullable(WITH_FRIEND_USERS.poll());
-                  case WITH_INCOME_REQUEST -> Optional.ofNullable(WITH_INCOME_REQUEST_USERS.poll());
-                  case WITH_OUTCOME_REQUEST -> Optional.ofNullable(WITH_OUTCOME_REQUEST_USERS.poll());
-                };
+                user = Optional.ofNullable(getUserType(ut).poll());
               }
               Allure.getLifecycle().updateTestCase(testCase -> {
                 testCase.setStart(new Date().getTime());
@@ -89,13 +84,17 @@ public class UsersQueueExtension implements
         context.getUniqueId(),
         Map.class);
     for (Map.Entry<UserType, StaticUser> e : map.entrySet()) {
-      switch (e.getKey().value()) {
-        case EMPTY -> EMPTY_USERS.add(e.getValue());
-        case WITH_FRIEND -> WITH_FRIEND_USERS.add(e.getValue());
-        case WITH_INCOME_REQUEST -> WITH_INCOME_REQUEST_USERS.add(e.getValue());
-        case WITH_OUTCOME_REQUEST -> WITH_OUTCOME_REQUEST_USERS.add(e.getValue());
-      }
+      getUserType(e.getKey());
     }
+  }
+
+  private Queue<StaticUser> getUserType(UserType ut) {
+    return switch (ut.value()) {
+      case EMPTY -> EMPTY_USERS;
+      case WITH_FRIEND -> WITH_FRIEND_USERS;
+      case WITH_INCOME_REQUEST -> WITH_INCOME_REQUEST_USERS;
+      case WITH_OUTCOME_REQUEST -> WITH_OUTCOME_REQUEST_USERS;
+    };
   }
 
   @Override
